4c51e766c03c8c0ed9ebf0d5f6dcee5383c1e345
thuanhaine <19521462@gm.uit.edu.vn>
2022-11-02 00:22:04 +0700

    test change project to react

---
+++ my-app/src/components/title/Title.jsx
@@ -0,0 +1,9 @@
+function Title() {
+    return (
+        <>
+         <div className="heading"><h1 className="neon" data-text="DRUMS PAD">DRUMS PAD</h1></div>
+        </>
+    )
+}
+
+export default Title;
