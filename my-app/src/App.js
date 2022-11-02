e0469cbbd2a3db65cf99c37e27358ae1f4bda00b
thuanhaine <19521462@gm.uit.edu.vn>
2022-11-02 22:08:02 +0700

    check commit branch day3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Gameplay from './components/gamePlay/Gameplay';
 import Title from './components/title/Title';
-import HomePage from './components/homePage/HomePage';
+// import HomePage from './components/homePage/HomePage';
 function App() {
   return (
     <>
     <Title/>
-    <HomePage/>
+    {/* <HomePage/> */}
     <Gameplay/>
     </>
   );
